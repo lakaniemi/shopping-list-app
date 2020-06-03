fa6896bc2fb7aa51909acf4d1fe2b54488be3b4b
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2020-06-03 13:38:46 +0300

    Add react-native-paper

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+
 import App from './src/App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Root() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Root);
